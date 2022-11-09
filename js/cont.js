0e13c9c50f55950d49277f1f9d7e794e7df3922c
bibnat <86243936+bibnat@users.noreply.github.com>
2022-11-08 22:24:42 -0300

    Add files via upload

---
+++ js/cont.js
@@ -0,0 +1,8 @@
+function vision()
+    {
+      document.body.style.backgroundColor= "black";
+      document.body.style.color= "white";
+      document.getElementById("header").style.backgroundColor = "black";
+      document.getElementById("nav").style.backgroundColor = "white";
+      document.getElementById("footer").style.backgroundColor = "black";
+    }
---
+++ js/ind.js
@@ -0,0 +1,21 @@
+function vision()
+    {
+      document.body.style.backgroundColor= "black";
+      document.body.style.color= "white";
+      document.getElementById("header").style.backgroundColor = "black";
+      document.getElementById("nav").style.backgroundColor = "white";
+      document.getElementById("footer").style.backgroundColor = "black";
+
+      document.getElementById("subrayado").style.color = "white";
+      document.getElementById("rojotitulo").style.color = "white";
+      document.getElementById("verdetitulo").style.color = "white";
+      document.getElementById("subrayado2").style.color = "white";
+      document.getElementById("mira").style.color = "white";
+      document.getElementById("lugar").style.color = "white";
+
+    }
+
+
+function bio() {
+    alert("Desarrolado por: M. Natalia Méndez/ Estudios: Bibliotecóloga/ Localidad: Rosario/ Correo: nataliacisc@gmail.com");
+    }
